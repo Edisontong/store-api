9fed06499265f6ca23eb5c2d0f98bc2bb2b93147
Edisontong <122298502+Edisontong@users.noreply.github.com>
2023-03-03 11:22:31 -0700

    changed price column

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,10 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render :index
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_174558) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_182054) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_174558) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.decimal "price", precision: 2, scale: 2
+    t.decimal "price", precision: 9, scale: 2
   end
 
 end
