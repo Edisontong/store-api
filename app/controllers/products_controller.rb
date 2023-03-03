f560738198f2959e71223688bdbf7741fa7dcb03
Edisontong <122298502+Edisontong@users.noreply.github.com>
2023-03-03 11:29:15 -0700

    made create action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,20 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find_by(id: params[:id])
   end
+
+  def create
+    @product = Product.create(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      quantity: params[:quantity],
+    )
+    render :show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
